c0e70c2b5b2f8422ade16b66248d9c24a1e29dd8
Ian <mus5002@aol.com>
2020-05-26 16:23:29 -0400

    refactored file structure

---
+++ js/data.js
@@ -0,0 +1,37 @@
+const notesArrayWithSharps = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#']
+const notesArrayWithFlats = ['a', 'bb', 'b', 'c', 'db', 'd', 'eb', 'e', 'f', 'gb', 'g', 'ab']
+const intervalsArray = ['1', 'b2', '2', 'b3', '3', '4', 'b5', '5', 'b6', '6', 'b7', '7']
+const chordStructures = [
+    {
+        name: 'power chord',
+        intervals: ['5']
+    },
+    {
+        name: 'major chord',
+        intervals: ['3', '5']
+    },
+    {
+        name: 'minor chord',
+        intervals: ['b3', '5']
+    },
+    {
+        name: 'aug',
+        intervals: ['3', 'b6']
+    },
+    {
+        name: 'dim',
+        intervals: ['b3', 'b5']
+    },
+    {
+        name: 'sus4',
+        intervals: ['4', '5']
+    },
+    {
+        name: 'sus2',
+        intervals: ['2', '5']
+    },
+    {
+        name: 'perfect fourth',
+        intervals: ['4']
+    }
+]
